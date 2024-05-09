9aea263823e34c2fa2d93b98d25e67eef8f8223b
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-05-09 22:31:50 +0530

    Day26

---
+++ DSA/Day22.py
@@ -0,0 +1,20 @@
+# find prime is 2d array 
+# def solve(N,M,arr):
+#     count = 0
+#     for i in range(N):
+#         for j in range(M):
+#             if arr[i][j]>1:
+#                 is_prime = True
+#                 for k in  range(2,arr[i][j]):
+#                     if arr[i][j] % k == 0:
+#                         is_prime = False
+#                         break
+#                 if is_prime:
+#                     count += 1
+                    
+#     print(count)       
+# def solve(N,M,arr):
+#     for i in range(M-1,-1,-1):
+#         for j in range(N):
+#             print(arr[j][i],end=" ")
+            
