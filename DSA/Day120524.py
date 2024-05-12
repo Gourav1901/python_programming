dce7fcd0123121f5121146a8f8f6d2a647829c9a
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-05-12 13:51:12 +0530

    Row with max 1s

---
+++ DSA/Day120524.py
@@ -0,0 +1,16 @@
+# class Solution:
+
+# 	def rowWithMax1s(self,arr, n, m):
+	    
+# 	    i = 0
+# 	    j = m-1
+# 	    res = -1
+	    
+# 	    while i < n and j >= 0:
+# 	        if arr[i][j] == 1:
+# 	            j -=1
+# 	            res = i
+# 	        else:
+# 	            i += 1
+# 	    return res
+
