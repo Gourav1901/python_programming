fcb1544a43afa0d37c5c8ac15f22cb5da2d06987
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-05-14 23:35:51 +0530

    Learn Input taking in python and 1 problem on it

---
+++ DSA/Day14_05_24.py
@@ -0,0 +1,39 @@
+# N, M = map(int, input().split())
+
+# arr = []
+# for i in range(N):
+#     arr1 = []
+#     arr.append(input())
+#     for j in range(M):
+#         arr1.append(input())
+#     arr.append(arr1)
+# print(arr)
+# n = int(input())
+# arr =[]
+# for i in range(n):
+#   arr.append(input())
+# for i in arr:
+#   print(i,end=" ")
+
+# n,m = map(int,input().split())
+# arr =[]
+# for i in range(n):
+#     arr.append(list(map(int,input().split())))
+# print(arr)
+
+#solve some qus 
+# N, M = map(int, input().split())
+
+# arr = []
+# for i in range(N):
+#     arr.append(list(map(int,input().split())))
+# for i in range(M):
+#     pro = 1
+#     for j in range(N):
+        
+#         pro *=(arr[j][i])
+#     print(pro)   
+    
+
+
+
